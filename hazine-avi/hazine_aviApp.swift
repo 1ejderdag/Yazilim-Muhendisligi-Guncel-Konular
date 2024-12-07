a25370478d76c711e09ff2451ec35c3b981ec831
Ejder Dag <ejdrdag@gmail.com>
2024-12-07 13:22:23 +0300

    first commit

---
+++ hazine-avi/hazine_aviApp.swift
@@ -0,0 +1,17 @@
+//
+//  hazine_aviApp.swift
+//  hazine-avi
+//
+//  Created by Ejder Dağ on 7.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct hazine_aviApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
