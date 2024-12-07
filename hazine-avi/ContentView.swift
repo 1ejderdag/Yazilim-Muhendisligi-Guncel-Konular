ec1fd7578f40519e5e88d17e6e4c48fa77540b5b
Ejder Dag <ejdrdag@gmail.com>
2024-12-07 14:54:34 +0300

    folder structured

--- hazine-avi/ContentView.swift
+++ hazine-avi/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+                
         }
-        .padding()
+        
     }
 }
 
